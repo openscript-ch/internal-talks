a733be05d748cb770b8abe36e1a8934b53ddfb23
Robin Bühler <git+r@obin.ch>
2023-12-13 15:19:52 +0000

    chore: enhance build index

--- scripts/build-index.js
+++ scripts/build-index.js
@@ -1,28 +1,42 @@
 import fs from 'node:fs/promises';
+import path from 'node:path';
 
-const TALKS_PATH = 'dist/';
-const OUTPUT_PATH = 'dist/index.html';
+const TALKS_PATH = 'talks/';
+const OUTPUT_PATH = 'dist';
+const PUBLIC_PATH = 'public/';
 const talks = [];
 
 const talksFiles = await fs.readdir(TALKS_PATH, { withFileTypes: true });
 for (const file of talksFiles) {
   if (file.isDirectory()) {
-    talks.push(file.name);
+    try {
+      const rawPackageJson = await fs.readFile(path.join(file.path, file.name, 'package.json'));
+      const packageJson = JSON.parse(rawPackageJson.toString());
+      talks.push({path: file.name, title: packageJson.title, author: packageJson.author});
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
 
-const createLinkList = (talks) => talks.map(talk => `<li><a href="./${talk}">${talk}</a></li>`).join('');
-await fs.writeFile(OUTPUT_PATH, `<!DOCTYPE html>
+const createLinkList = (talks) => talks.map(talk => `<li><a href="./${talk.path}">${talk.title} by ${talk.author.name}</a></li>`).join('');
+await fs.writeFile(path.join(OUTPUT_PATH, 'index.html'), `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Our internal talks</title>
+    <link rel="stylesheet" href="styles.css" />
   </head>
   <body>
+    <img src="logo.png" alt="openscript logo" />
+    <h1>Internal talks</h1>
     <ul>
-      ${createLinkList(talks)}
+      ${createLinkList(talks.reverse())}
     </ul>
   </body>
 </html>
 `);
+
+await fs.copyFile(path.join(PUBLIC_PATH, 'styles.css'), path.join(OUTPUT_PATH, 'styles.css'));
+await fs.copyFile(path.join(PUBLIC_PATH, 'logo.png'), path.join(OUTPUT_PATH, 'logo.png'));
